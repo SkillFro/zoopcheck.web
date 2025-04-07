040221885c72eb51a22226abbf3d06f186861881
Suresh-Fullstackwebdeveloper <sureshdhanasekardeveloper@gmail.com>
2025-04-07 12:33:59 +0530

    add header

---
+++ components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-full h-[95px] bg-white py-[8px] px-[12px]">
+    <img src="/public/images/logo.jpg" alt="" width="150px" height="53px" />
+    <p class="text-[#ff4848]"><a href="#">Home</a></p>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import { Header } from "#components";
+</script>
+
 <template>
   <div>
+    <Header />
     <h1>Hello</h1>
   </div>
 </template>

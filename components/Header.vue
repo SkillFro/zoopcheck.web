6c093f8a693a684511e6e16b52bc0c07497a7479
Suresh-Fullstackwebdeveloper <sureshdhanasekardeveloper@gmail.com>
2025-04-07 13:28:19 +0530

    fix header

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,58 @@
 <template>
-  <div class="w-full h-[95px] bg-white py-[8px] px-[12px]">
+  <div
+    class="w-full h-[95px] bg-white py-[8px] px-[12px] flex justify-center items-center gap-[282px] fixed top-0"
+  >
     <img src="/public/images/logo.jpg" alt="" width="150px" height="53px" />
-    <p class="text-[#ff4848]"><a href="#">Home</a></p>
+    <div class="flex items-center gap-[48px]">
+      <div class="flex items-center" v-for="nav in navs">
+        <p
+          class="hover:text-[#2966f4] text-[#212529] text-[16px] hover:underline hover:underline-offset-8 hover:decoration-[#2966f4] hover:decoration-2 leading-[24px]"
+        >
+          <NuxtLink :to="nav.path">{{ nav.title }} </NuxtLink>
+        </p>
+      </div>
+      <div class="flex items-center gap-[16px]">
+        <button
+          class="py-[6px] px-[12px] bg-[#2966f4] text-white text-[16px] rounded-[0.375rem]"
+        >
+          Submit Resume
+        </button>
+        <button
+          class="py-[6px] px-[12px] bg-[#0a2d52] text-white text-[16px] rounded-[0.375rem]"
+        >
+          Hire Canditates
+        </button>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      navs: [
+        {
+          title: "Home",
+          path: "/",
+        },
+        {
+          title: "About",
+          path: "/",
+        },
+        {
+          title: "Current Openings",
+          path: "/",
+        },
+        {
+          title: "Tech Stories",
+          path: "/",
+        },
+        {
+          title: "Contact",
+          path: "/",
+        },
+      ],
+    };
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,27 @@ import { Header } from "#components";
 
 <template>
   <div>
-    <Header />
-    <h1>Hello</h1>
+    <div
+      class="bg-[url('/images/hero.jpg')] bg-black/60 mt-24 bg-blend-overlay bg-cover bg-no-repeat w-full h-screen flex flex-col justify-center items-center text-white"
+    >
+      <div class="max-w-[1047px]">
+        <h1>
+          Unlocking your career potential <br />
+          with ZoopCheck
+        </h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, nemo
+        unde assumenda ad inventore incidunt at nobis iste aliquid. Quidem modi
+        iure saepe rem excepturi ipsum nostrum sed sapiente! Aspernatur? Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Commodi, nemo unde
+        assumenda ad inventore incidunt at nobis iste aliquid. Quidem modi iure
+        saepe rem excepturi ipsum nostrum sed sapiente! Aspernatur? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Commodi, nemo unde
+        assumenda ad inventore incidunt at nobis iste aliquid. Quidem modi iure
+        saepe rem excepturi ipsum nostrum sed sapiente! Aspernatur? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Commodi, nemo unde
+        assumenda ad inventore incidunt at nobis iste aliquid. Quidem modi iure
+        saepe rem excepturi ipsum nostrum sed sapiente! Aspernatur?
+      </div>
+    </div>
   </div>
 </template>

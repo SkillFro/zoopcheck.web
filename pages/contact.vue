6ecb169d55138bde946d895d2189afeeb8e12b8b
priya09-collab <priyadev0912@gmail.com>
2025-04-08 17:29:25 +0530

    feat:contact

---
+++ pages/contact.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="relative flex flex-col items-center justify-center">
+      <h1
+        class="font-semibold md:text-[35px] text-[30px] leading-[24px] text-[#212529] text-center mb-[50px] mt-20"
+      >
+        Contact Us
+      </h1>
+      <div
+        class="md:w-[180px] w-[120px] bg-[#2966f4] h-0.5 md:-mt-6 -mt-8"
+      ></div>
+    </div>
+    <div>
+      <div
+        class="w-[614px] h-[633px] drop-shadow-2xl flex flex-col items-center justify-center mx-auto bg-white rounded-[0.375rem] mt-10 mb-10"
+      >
+        <img
+          src="/public/images/logo.jpg"
+          alt="logo"
+          width="150px"
+          height="53px"
+          class="-mt-10"
+        />
+        <div
+          class="flex flex-col items-start justify-start px-[60px] mt-[48px]"
+        >
+          <h2 class="text-[32px] font-medium leading-[38.4px] text-[#2966f4]">
+            Partner with us to envision a brand new future for your business.
+          </h2>
+          <div class="flex flex-col gap-5 mt-[48px]">
+            <p
+              class="text-[16px] leading-[19.2px] font-medium text-[#212529] underline"
+            >
+              Emai
+            </p>
+            <p class="text-[16px] leading-[25px] text-[#555a64] font-normal">
+              hello@zoopcheck.com
+            </p>
+          </div>
+          <div class="flex flex-col gap-5 mt-10">
+            <p
+              class="text-[16px] leading-[19.2px] font-medium text-[#212529] underline"
+            >
+              Reach us
+            </p>
+            <p class="text-[16px] leading-[25px] text-[#555a64] font-normal">
+              +91 78457 41717
+            </p>
+          </div>
+          <div class="flex flex-col gap-5 mt-10">
+            <p
+              class="text-[16px] leading-[19.2px] font-medium text-[#212529] underline"
+            >
+              Location
+            </p>
+            <p class="text-[16px] leading-[25px] text-[#555a64] font-normal">
+              Chennai
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        class="w-[614px] h-[633px] drop-shadow-2xl flex flex-col items-center justify-center mx-auto bg-[#2966f4] rounded-[0.375rem] mt-10 mb-10"
+      >
+        <h4 class="text-[30px] leading-[35px] text-white font-medium">
+          Get in Touch
+        </h4>
+      </div>
+    </div>
+  </div>
+</template>

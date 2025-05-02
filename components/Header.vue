fc924ebe48b45c784ca1f88e208976f9d7cb4418
SakthiShivan <69505534+SakthiShivanDev@users.noreply.github.com>
2025-05-02 16:32:30 +0530

    refactor: code

--- components/Header.vue
+++ components/Header.vue
@@ -2,94 +2,85 @@
   <!-- xl device navbar -->
   <div class="hidden lg:block">
     <div
-      class="w-full px-4 py-6 bg-white/50 shadow-xs backdrop-blur-md fixed top-0 flex justify-center items-center z-10"
-    >
-      <div
-        class="xl:max-w-[1120px] w-full mx-auto flex items-center justify-between "
-      >
-        <img src="/public/images/logo.svg" alt="" width="150px" />
+      class="w-full px-4 py-6 bg-white/50 shadow-xs backdrop-blur-md fixed top-0 flex justify-center items-center z-10">
+      <div class="xl:max-w-[1120px] w-full mx-auto flex items-center justify-between ">
+        <a href="/">
+          <img src="/public/images/logo.svg" alt="" width="150px" />
+        </a>
         <div class="flex items-center xl:gap-[64px] gap-[30px]">
           <div class="flex items-center xl:gap-[16px] gap-[10px]">
-            <div
-              v-for="nav in navs"
-              :key="nav"
+            <div v-for="nav in navs" :key="nav"
               class="hover:text-[#086BD8] font-semibold  text-[16px] hover:underline hover:underline-offset-[12px] hover:decoration-[#086BD8] hover:decoration-2 leading-[24px] px-2"
-              :class="
-                $route.path === nav.path?'  text-[#086BD8] underline underline-offset-[12px] decoration-[#086BD8] decoration-2':'text-gray-800'
-              "
-            >
+              :class="getRoute(nav.path)  ? '  text-[#086BD8] underline underline-offset-[12px] decoration-[#086BD8] decoration-2' : 'text-gray-800'
+                ">
               <NuxtLink :to="nav.path">
                 {{ nav.name }}
               </NuxtLink>
             </div>
           </div>
-          <div v-if="authState.status!=='authenticated'" class="flex items-center gap-[16px]">
+          <div v-if="authState.status !== 'authenticated'" class="flex items-center gap-[16px]">
             <NuxtLink to="/signin">
-              <button
-                class="py-[6px] px-[12px] cursor-pointer bg-[#086BD8] text-white text-[16px] rounded-[0.375rem]"
-              >
+              <button class="py-[6px] px-[12px] cursor-pointer bg-[#086BD8] text-white text-[16px] rounded-[0.375rem]">
                 Sign In
               </button>
             </NuxtLink>
-            <button
-              class="py-[6px] px-[12px] border cursor-pointer border-[#086BD8] text-[#086BD8] text-[16px] rounded-[0.375rem]"
-            >
+            <NuxtLink to="/signup"
+              class="py-[6px] px-[12px] border cursor-pointer border-[#086BD8] text-[#086BD8] text-[16px] rounded-[0.375rem]">
               Sign Up
-            </button>
+            </NuxtLink>
           </div>
-          <NuxtLink v-else to="/profile">
-              <!-- <button
+          <div v-else class="relative">
+            <!-- <button
                 class="py-[6px] px-[12px] bg-[#086BD8] text-white text-[16px] rounded-[0.375rem]"
               >
                 Profile
               </button> -->
-              <img class="w-10 h-10 rounded-full" :src="authState.data.user.profile" alt="">
-            </NuxtLink>
+            <img @click="() => toggle = !toggle" class="w-10 h-10 rounded-full" :src="authState.data.user.profile" alt="">
+            <div v-if="toggle"
+              class="bg-white shadow flex flex-col absolute z-10 right-2 p-2 top-10 gap-2 items-end mt-2">
+              <NuxtLink to="/profile" @click="() => toggle = !toggle" class="cursor-pointer px-4">Account</NuxtLink>
+
+              <NuxtLink @click="logout" class="text-red-500 px-4 cursor-pointer">Log Out</NuxtLink>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
   </div>
 
   <!-- sm device navbar -->
-  <div class="fixed top-0 z-10 shadow-xs backdrop-blur-md bg-white/50 block w-full lg:hidden">
-    <div class="flex items-center justify-between p-4 bg- lg:px-10">
-      <img src="/public/images/logo.svg" alt="logo" class="w-[150px]" />
-      <button
-        @click="toggleNav()"
-        class="px-[12px] rounded-md py-[4px] border border-[#00000026] focus:border-black focus:border-2"
-      >
-        <span
-          ><img src="/public/images/bar.svg" width="30px" height="30px" alt=""
-        /></span>
+  <div class="fixed top-0 z-10 block w-full py-4 bg-white lg:hidden">
+    <div class="flex items-center justify-between px-4 lg:px-10">
+      <a href="/">
+        <img src="/public/images/logo.svg" alt="logo" class="w-[150px]" />
+      </a>
+      <button @click="toggleNav()"
+        class="px-[12px] rounded-md py-[4px] border border-[#00000026] focus:border-black focus:border-2">
+        <span><img src="/public/images/bar.svg" width="30px" height="30px" alt="" /></span>
       </button>
     </div>
-    <div v-if="nav" class="w-full p-10 bg-white">
+    <div v-if="nav" class="w-full h-screen  bg-black/70">
+    <div class="bg-white p-10">
       <div class="flex flex-col items-end justify-center gap-[40px]">
-        <div
-              v-for="nav in navs"
-              :key="nav"
-              class="hover:text-[#086BD8] font-semibold  text-[16px] hover:underline hover:underline-offset-[12px] hover:decoration-[#086BD8] hover:decoration-2 leading-[24px] px-2"
-              :class="
-                $route.path === nav.path?'  text-[#086BD8] underline underline-offset-[12px] decoration-[#086BD8] decoration-2':'text-gray-800'
-              "
-            >
-              <NuxtLink :to="nav.path">
-                {{ nav.name }}
-              </NuxtLink>
-            </div>
+        <div v-for="nav in navs" :key="nav"
+          class="hover:text-[#086BD8] font-semibold  text-[16px] hover:underline hover:underline-offset-[12px] hover:decoration-[#086BD8] hover:decoration-2 leading-[24px] px-2"
+          :class="getRoute(nav.path) ? '  text-[#086BD8] underline underline-offset-[12px] decoration-[#086BD8] decoration-2' : 'text-gray-800'
+            ">
+          <NuxtLink :to="nav.path">
+            {{ nav.name }}
+          </NuxtLink>
+        </div>
         <div class="flex flex-col gap-[16px]">
-          <button
-            class="py-[6px] px-[12px] bg-[#086BD8] text-white text-[16px] rounded-[0.375rem]"
-          >
+          <button class="py-[6px] px-[12px] bg-[#086BD8] text-white text-[16px] rounded-[0.375rem]">
             Sign In
           </button>
-          <button
-            class="py-[6px] px-[12px] bg-[#0a2d52] text-white text-[16px] rounded-[0.375rem]"
-          >
+          <button class="py-[6px] px-[12px] bg-[#0a2d52] text-white text-[16px] rounded-[0.375rem]">
             Sign Up
           </button>
         </div>
       </div>
+    </div>
     </div>
   </div>
 </template>
@@ -98,7 +89,8 @@ export default {
   data() {
     return {
       nav: false,
-      authState:useAuth(),
+      authState: useAuth(),
+      toggle: false,
       navs: [
         {
           name: "Home",
@@ -117,7 +109,7 @@ export default {
           name: "About",
           path: "/about",
         },
-        
+
         {
           name: "Contact",
           path: "/contact",
@@ -134,6 +126,13 @@ export default {
       this.nav = false;
       document.body.style.overflow = "";
     },
+    logout() {
+      this.toggle = !this.toggle
+      useAuth().signOut({ callbackUrl: '/' })
+    },
+    getRoute(path){
+      return this.$route.path ===path||this.$route.fullPath.replace('/','').split('/').includes(path.replace('/',''))
+    }
   },
   watch: {
     $route() {
--- pages/signin.vue
+++ pages/signin.vue
@@ -1,58 +1,80 @@
 <template>
-<div class="flex min-h-screen mt-30 flex-col justify- px-6 py-12 lg:px-8">
-  <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <img class="mx-auto h-20 w-auto" src="/public/images/logo.svg" alt="Your Company">
-    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-  </div>
-
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <div class="space-y-6" >
-      <div>
-        <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
-        <div class="mt-2">
-          <input v-model="email" type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#086BD8] sm:text-sm/6">
-        </div>
-      </div>
-
-      <div>
-        <div class="flex items-center justify-between">
-          <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-          <div class="text-sm">
-            <a href="#" class="font-semibold text-[#086BD8] hover:text-[#086BD8]">Forgot password?</a>
-          </div>
-        </div>
-        <div class="mt-2">
-          <input v-model="password" type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#086BD8] sm:text-sm/6">
-        </div>
-      </div>
-
-      <div>
-        <button @click="login" type="submit" class="flex w-full justify-center rounded-md bg-[#086BD8] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-[#086BD8] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#086BD8]">Sign in</button>
-      </div>
+  <div class="flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <a href="/">
+        <img class="mx-auto h-20 w-auto" src="/public/images/logo.svg" alt="Your Company">
+      </a>
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
     </div>
 
-    <p class="mt-10 text-center text-sm/6 text-gray-500">
-      Not a member?
-      <a href="/signUp" class="font-semibold text-[#086BD8] hover:text-[#086BD8]">Sign Up</a>
-    </p>
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div class="space-y-6">
+        <div>
+          <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+          <div class="mt-2">
+            <input v-model="email" type="email" name="email" id="email" autocomplete="email" required
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#086BD8] sm:text-sm/6">
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
+            <div class="text-sm">
+              <!-- <a href="#" class="font-semibold text-[#086BD8] hover:text-[#086BD8]">Forgot password?</a> -->
+            </div>
+          </div>
+          <div class="mt-2">
+            <input v-model="password" type="password" name="password" id="password" autocomplete="current-password"
+              required
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#086BD8] sm:text-sm/6">
+          </div>
+        </div>
+
+        <div>
+          <button @click="login" type="submit"
+            class="flex w-full justify-center rounded-md bg-[#086BD8] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-[#086BD8] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#086BD8]">
+            <span v-if="!loading">Sign in</span>
+            <span v-else class="border-2 border-white border-l-slate-400 w-5 h-5 rounded-full animate-spin"></span>
+          </button>
+        </div>
+      </div>
+
+      <p class="mt-10 text-center text-sm/6 text-gray-500">
+        Not a member?
+        <a href="/signUp" class="font-semibold text-[#086BD8] hover:text-[#086BD8]">Sign Up</a>
+      </p>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-export default{
-  setup(){
-    definePageMeta({layout:'primary'})
+export default {
+  setup() {
+    definePageMeta({
+      layout: 'primary', middleware: 'sidebase-auth', auth: {
+        unauthenticatedOnly: true,
+        navigateUnauthenticatedTo: '/profile'
+      }
+    })
   },
-  data(){
+  data() {
     return {
-      email:"",
-      password:""
+      loading: false,
+      email: "",
+      password: ""
     }
   },
-  methods:{
-    login(){
-      useAuth().signIn({email:this.email,password:this.password},{ callbackUrl: '/'})
+  methods: {
+    async login() {
+      this.loading = true;
+      await useAuth().signIn({ email: this.email, password: this.password }, { callbackUrl: '/' }).then((res) => {
+        push.success({title:'Success',message:'You logged in successfully !'})
+      }).catch((err) => {
+        push.error({title:'Error',message:'Invalid Email or Password'})
+      })
+
+      this.loading = false
     }
   }
 }

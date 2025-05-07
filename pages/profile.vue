1f61515263f77d122229072057fa758dbae69bb8
Suresh-Fullstackwebdeveloper <sureshdhanasekardeveloper@gmail.com>
2025-05-07 12:44:50 +0530

    create updateform

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,32 +1,40 @@
 <template>
-  <div class="max-w-[1080px] mx-auto px-4 lg:px-0 mt-32">
-    <div class="flex gap-20 justify-between">
-      <div class="sticky top-20">
-        <div class="flex sticky top-32 flex-col w-[250px] gap-5">
+  <div
+    class="md:max-w-[1080px] max-w-full md:mx-auto md:px-4 lg:px-0 md:mt-32 mt-24"
+  >
+    <div class="md:flex md:flex-row flex-col gap-20 justify-between">
+      <div class="md:sticky md:top-20 z-10">
+        <div
+          class="flex md:sticky md:top-32 flex-col md:w-[250px] w-full bg-[#f8f9fa] md:bg-none pb-10 px-4"
+        >
           <p class="text-xl font-semibold pb-10">Account Settings</p>
-          <NuxtLink
-            v-for="(tab, index) in tabs"
-            :key="index"
-            :to="`/profile?tab=${tab.value}`"
-            class="text-left px-5 py-2 rounded-lg cursor-pointer text-lg font-semibold whitespace-nowrap"
-            :class="[
-              selectedTab === tab.value ||
-              (selectedTab === 'applications' && tab.value === 'jobs')
-                ? 'bg-[#086BD8] text-white'
-                : ' text-gray-800',
-              '',
-            ]"
-          >
-            {{ tab.name }}
-          </NuxtLink>
+          <div class="flex md:flex-col flex-row gap-2">
+            <NuxtLink
+              v-for="(tab, index) in tabs"
+              :key="index"
+              :to="`/profile?tab=${tab.value}`"
+              class="text-left px-5 py-2 rounded-lg cursor-pointer text-lg font-semibold whitespace-nowrap"
+              :class="[
+                selectedTab === tab.value ||
+                (selectedTab === 'applications' && tab.value === 'jobs')
+                  ? 'bg-[#086BD8] text-white'
+                  : ' text-gray-800',
+                '',
+              ]"
+            >
+              {{ tab.name }}
+            </NuxtLink>
+          </div>
         </div>
       </div>
-      <div class="flex-1">
+      <div class="flex-1 p-2">
         <section v-if="selectedTab === 'profile'" class="flex-1">
-          <div
-            v-if="user !== null"
-            class="flex flex-col items-center lg:flex-row gap-6 justify-between"
-          >
+          <!-- <div class="py-2 px-4 mb-4 bg-blue-100 rounded-md text-blue-500 text-sm">Sorry your account not verified yet! Complete your profile that us to verify</div> -->
+          <div v-if="user !== null" class="flex px-4 gap-6">
+            <!-- <div>
+            <p class="flex-1 text-lg font-semibold">Personal Information</p>
+            <p class="mt-1 text-sm/6 text-gray-600">Use a permanent address where you can receive mail.</p>
+          </div> -->
             <div class="flex-1 space-y-12">
               <div
                 v-if="!user.approved"
@@ -36,22 +44,38 @@
                 profile to proccess it ...
               </div>
               <div class="pb-12">
-                <!-- <h2 class="text-base/7 font-semibold text-gray-900">Profile</h2> -->
-
-                <!-- <p class="mt-1 text-sm/6 text-gray-600">
-                  This information will be displayed publicly so be careful what
-                  you share.
-                </p> -->
+                <div class="flex justify-between items-start">
+                  <div>
+                    <h2 class="text-base/7 font-semibold text-gray-900">
+                      Profile
+                    </h2>
+                    <p class="mt-1 text-sm/6 text-gray-600 w-[250px] md:w-full">
+                      This information will be displayed publicly so be careful
+                      what you share.
+                    </p>
+                  </div>
+                  <div
+                    @click="openCandidate()"
+                    class="flex gap-1 cursor-pointer"
+                  >
+                    <img
+                      src="/public/icons/edit.svg"
+                      alt="edit"
+                      class="w-5 h-5"
+                    />
+                    <p class="text-[#1447e6] text-sm">Edit</p>
+                  </div>
+                </div>
 
                 <div
                   class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6"
                 >
                   <div class="col-span-full">
-                    <!-- <label
+                    <label
                       for="photo"
                       class="block text-sm/6 font-medium text-gray-900"
                       >Comapany Logo</label
-                    > -->
+                    >
                     <div class="mt-4 w-12 flex items-center gap-x-3">
                       <img
                         class="object-cover rounded-full w-12 h-12"
@@ -62,22 +86,6 @@
                         "
                         alt=""
                       />
-                      <label
-                        for="file-upload"
-                        class="relative cursor-pointer rounded-md bg-white font-semibold text-[#086BD8]"
-                      >
-                        <div
-                          class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50"
-                        >
-                          Change
-                        </div>
-                        <input
-                          id="file-upload"
-                          name="file-upload"
-                          type="file"
-                          class="sr-only"
-                        />
-                      </label>
                     </div>
                   </div>
                   <div class="sm:col-span-4">
@@ -728,6 +736,179 @@
         </div>
       </div>
     </div>
+
+    <!-- canditate  form -->
+    <div
+      v-if="candidateProfile"
+      class="bg-[#00000095] w-full fixed inset-0 z-50 p-2 flex flex-col justify-center items-center h-[100vh]"
+    >
+      <div
+        class="md:w-[600px] bg-white w-full p-5 flex flex-col gap-5 overflow-y-auto rounded-lg"
+      >
+        <div class="col-span-full">
+          <label for="photo" class="block text-sm/6 font-medium text-gray-900"
+            >Comapany Logo</label
+          >
+          <div class="mt-4 w-12 flex items-center gap-x-3">
+            <img
+              class="object-cover rounded-full w-12 h-12"
+              :src="
+                user.profile === ''
+                  ? 'https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3383.jpg'
+                  : user.profile
+              "
+              alt=""
+            />
+            <label
+              for="file-upload"
+              class="relative cursor-pointer rounded-md bg-white font-semibold text-[#086BD8]"
+            >
+              <div
+                class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50"
+              >
+                Change
+              </div>
+              <input
+                id="file-upload"
+                name="file-upload"
+                type="file"
+                class="sr-only"
+              />
+            </label>
+          </div>
+        </div>
+        <div class="sm:col-span-4">
+          <label
+            for="username"
+            class="block text-sm/6 font-medium text-gray-900"
+            >Company Name</label
+          >
+          <div class="mt-4">
+            <div
+              class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-[#086BD8]"
+            >
+              <input
+                v-model="user.name"
+                type="text"
+                name="username"
+                id="username"
+                class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                placeholder="ZoopCheck Privated Limited"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="sm:col-span-4">
+          <label for="about" class="block text-sm/6 font-medium text-gray-900"
+            >About</label
+          >
+          <div class="mt-4">
+            <textarea
+              v-model="user.description"
+              name="about"
+              id="about"
+              rows="6"
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#086BD8] sm:text-sm/6"
+            ></textarea>
+          </div>
+          <p class="mt-3 text-sm/6 text-gray-600">
+            Write a few sentences about yourself.
+          </p>
+        </div>
+
+        <div class="sm:col-span-4">
+          <label for="email" class="block text-sm/6 font-medium text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-4">
+            <input
+              v-model="user.email"
+              id="email"
+              placeholder="hello@zoopcheck.com"
+              name="email"
+              type="email"
+              autocomplete="email"
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#086BD8] sm:text-sm/6"
+            />
+          </div>
+        </div>
+        <div class="sm:col-span-4">
+          <label for="email" class="block text-sm/6 font-medium text-gray-900"
+            >Mobile Number</label
+          >
+          <div class="mt-4">
+            <input
+              v-model="user.number"
+              id="email"
+              placeholder="1234567890"
+              name="email"
+              type="text"
+              autocomplete="email"
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#086BD8] sm:text-sm/6"
+            />
+          </div>
+        </div>
+        <div class="sm:col-span-4">
+          <label for="email" class="block text-sm/6 font-medium text-gray-900"
+            ><span v-if="user.role === 'recruiter'">Company website</span
+            ><span v-if="user.role === 'candidate'"
+              >Portfolio website or LinkedIn Profile</span
+            ></label
+          >
+          <div class="mt-4">
+            <input
+              v-model="user.website"
+              id="email"
+              placeholder="https://zoopcheck"
+              name="email"
+              type="text"
+              autocomplete="email"
+              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-[#086BD8] sm:text-sm/6"
+            />
+          </div>
+        </div>
+        <div class="flex items-center gap-5 justify-end w-full">
+          <button
+            @click="closeCandidate()"
+            class="py-2 px-4 rounded bg-gray-200 text-gray-900 w-fit cursor-pointer"
+          >
+            <span>Cancel</span>
+          </button>
+
+          <button
+            @click="updateProfile()"
+            class="py-2 px-4 rounded bg-[#086BD8] text-white w-fit cursor-pointer"
+            :disabled="loading"
+          >
+            <span v-if="loading">Processing...</span>
+            <span v-else>Update Profile</span>
+          </button>
+        </div>
+        <!-- <div class="col-span-full">
+                <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Cover photo</label>
+                <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                  <div class="text-center">
+                    <svg class="mx-auto size-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor"
+                      aria-hidden="true" data-slot="icon">
+                      <path fill-rule="evenodd"
+                        d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+                        clip-rule="evenodd" />
+                    </svg>
+                    <div class="mt-4 flex text-sm/6 text-gray-600">
+                      <label for="file-upload"
+                        class="relative cursor-pointer rounded-md bg-white font-semibold text-[#086BD8] focus-within:ring-2 focus-within:ring-[#086BD8] focus-within:ring-offset-2 focus-within:outline-hidden hover:text-[#086BD8]">
+                        <span>Upload a file</span>
+                        <input id="file-upload" name="file-upload" type="file" class="sr-only">
+                      </label>
+                      <p class="pl-1">or drag and drop</p>
+                    </div>
+                    <p class="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                  </div>
+                </div>
+              </div> -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -741,6 +922,7 @@ export default {
   },
   data() {
     return {
+      candidateProfile: false,
       route: useRoute(),
       selectedTab: useRoute().query.tab ?? "profile",
       user: null,
@@ -812,6 +994,14 @@ export default {
     },
   },
   methods: {
+    openCandidate() {
+      this.candidateProfile = true;
+      document.body.style.overflow = "hidden";
+    },
+    closeCandidate() {
+      this.candidateProfile = false;
+      document.body.style.overflow = "";
+    },
     toggle(index) {
       this.toggleIndex = this.toggleIndex === index ? null : index;
     },
@@ -846,6 +1036,34 @@ export default {
       //   push.info({ title: 'Info', message: "Sorry your account not verified yet !", props: {} })
       // }
     },
+    async updateProfile() {
+      const response = await this.$apiFetch(
+        `/${useAuth().data.value.user.role}/me`,
+        {
+          method: "PUT",
+          body: this.user,
+        }
+      );
+      if (response.success) {
+        push.success({ title: "Success", message: response.message });
+        this.openform();
+        this.getJobs();
+        this.formData = {
+          title: "",
+          description: "",
+          category: "",
+          openings: "",
+          experience: "",
+          workMode: "",
+          location: "",
+        };
+      } else {
+        push.error({ title: "Error", message: response.message });
+      }
+      this.candidateProfile = false;
+      document.body.style.overflow = "";
+    },
+
     async getJobs() {
       const response = await this.$apiFetch(
         `/recruiter/${useAuth().data.value.user.id}/posts`
